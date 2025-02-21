e3ad1d8640a84c4fafe12c586c3d9c9e7a4393ca
Yiğit KABAK <yigitkabak@proton.me>
2025-02-21 12:01:25 +0300

    Create app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>aperture</h1>
+      </div>
+      <div className="subheader">
+        <p>The only hub for .huh, fenrir and many more projects! a product of valor.</p>
+      </div>
+      <nav>
+        <Link href="https://github.com/outpoot" className="nav-link">github</Link>
+      </nav>
+      <footer>
+        <p>a yigit production.</p>
+      </footer>
+    </div>
+  );
+}
